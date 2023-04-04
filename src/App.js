e4bfcd6cbd21e3b5d229e7460d058b9c54cb144b
hakim-arbi <hakimarbi525@gmail.com>
2023-04-04 23:25:49 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello everyone!</p>
+        <p>Hello Hakimos</p>
       </header>
     </div>
   );
